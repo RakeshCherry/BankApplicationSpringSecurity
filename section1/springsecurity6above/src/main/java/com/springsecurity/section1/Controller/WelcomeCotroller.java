5d9ea5dcfe1c9b25b4417c3b72df1717233b3369
RAKESH M B <rakeshbhavimani1@gmail.com>
2024-07-26 20:06:45 +0530

    section 1 introduction to SpringSecurity

---
+++ section1/springsecurity6above/src/main/java/com/springsecurity/section1/Controller/WelcomeCotroller.java
@@ -0,0 +1,13 @@
+package com.springsecurity.section1.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WelcomeCotroller {
+
+    @GetMapping("/welcome")
+    public String sayWelcome(){
+        return "Welcome to Spring Application with out security";
+    }
+}
