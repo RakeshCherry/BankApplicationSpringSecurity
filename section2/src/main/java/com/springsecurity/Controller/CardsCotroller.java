0dc05fa8b3accb539db25c8ef6d249060686a4cd
RAKESH M B <rakeshbhavimani1@gmail.com>
2024-07-26 22:51:59 +0530

    started new Section 2

---
+++ section2/src/main/java/com/springsecurity/Controller/CardsCotroller.java
@@ -0,0 +1,13 @@
+package com.springsecurity.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CardsCotroller {
+
+    @GetMapping("/myCards")
+    public String getCardsDetails(){
+        return "Here are the Cards details from the DB";
+    }
+}
