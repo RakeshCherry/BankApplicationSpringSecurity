729c7d941e08dced0319f6df67d2f616c5aeb382
RAKESH M B <rakeshbhavimani1@gmail.com>
2024-07-28 15:21:07 +0530

    in this added CompromisedPasswordChecker method

--- section3/Notes.md
+++ section3/Notes.md
@@ -12,10 +12,16 @@ if we not use encoder it will be saved as plane text in the brower memory or sto
         c. Pbkdf2 etc
     * In these BCryptPasswordEncoder is a defalut method
 
-# BCryptPasswordEncoder implements PasswordEncoder
+ # BCryptPasswordEncoder implements PasswordEncoder
 
-
-[//]: # ( extra things)
+[//]: ( extra things)
     to check or generate BCryptPasswordEncoder we can check in the website given below
         Bcrypt-Generator.com
     we can Encrypt or Decrypt in this website
+
+ # CompromisedPasswordChecker method
+    * not accepts anyinput
+    * checks whether the password is compromised or not returns the non-null CompromisedPasswordDecistion 
+    * @NonNull annotaion CompromisedPasswordDecision check(String password);
+ 
+ # HaveIBeenPwnedRestApiPasswordChecker
--- section3/src/main/java/com/springsecurity/config/ProjectSecurityConfig.java
+++ section3/src/main/java/com/springsecurity/config/ProjectSecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
 import org.springframework.security.authentication.jaas.memory.InMemoryConfiguration;
+import org.springframework.security.authentication.password.CompromisedPasswordChecker;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
@@ -15,6 +16,7 @@ import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.password.HaveIBeenPwnedRestApiPasswordChecker;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
@@ -44,12 +46,13 @@ public class ProjectSecurityConfig {
     @Bean
     public UserDetailsService userDetailsService() {
         UserDetails user = User.withUsername("user")
-                .password("{noop}12345")
+                .password("{noop}user_@12345")
                 .authorities("read")
                 .build();
         UserDetails admin = User.withUsername("admin")
 //                the below password is encrypted Hash code of BCrypt and used prefix {bcrypt} its good practice to use prefix
-                .password("{bcrypt}$2a$12$UNlLvqOMkWvCKTwltPKk1uOoznL9rCpTc8RK8loLCisULkWA.oJvq")
+//                as we used compromisedPasswordChecker method we changed the below admin password as "admin_@1122" and encrypted it
+                .password("{bcrypt}$2a$12$mNWjGgxh3ep8zOcoCsgl1.i6cC.a0MkfusKiQZoYobJvm3i16pXY6")
                 .authorities("admin")
                 .build();
         return new InMemoryUserDetailsManager(user, admin);
@@ -58,6 +61,13 @@ public class ProjectSecurityConfig {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+
+    @Bean
+//    this below method not allows to create simple passwords to the users which may hack by the hackers
+//    this features introduces from SpringSecurity 6.3 version
+    public CompromisedPasswordChecker compromisedPasswordChecker(){
+        return new HaveIBeenPwnedRestApiPasswordChecker();
     }
 }
 
