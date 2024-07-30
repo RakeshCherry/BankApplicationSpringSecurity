a5c24c72bc320b108a298b7dfbe1eedd2190046e
RAKESH M B <rakeshbhavimani1@gmail.com>
2024-07-30 20:52:50 +0530

    connect docker and sqlectron bankapplication db

--- section4/src/main/java/com/springsecurity/config/ProjectSecurityConfig.java
+++ section4/src/main/java/com/springsecurity/config/ProjectSecurityConfig.java
@@ -1,22 +1,17 @@
 package com.springsecurity.config;
 
-import org.springframework.boot.autoconfigure.security.SecurityProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.annotation.Order;
-import org.springframework.security.authentication.jaas.memory.InMemoryConfiguration;
 import org.springframework.security.authentication.password.CompromisedPasswordChecker;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.bcrypt.BCrypt;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.password.HaveIBeenPwnedRestApiPasswordChecker;
+
+import javax.sql.DataSource;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
@@ -44,18 +39,25 @@ public class ProjectSecurityConfig {
 
     //    to configure userDetailsService below is the code
     @Bean
-    public UserDetailsService userDetailsService() {
-        UserDetails user = User.withUsername("user")
-                .password("{noop}user_@12345")
-                .authorities("read")
-                .build();
-        UserDetails admin = User.withUsername("admin")
-//                the below password is encrypted Hash code of BCrypt and used prefix {bcrypt} its good practice to use prefix
-//                as we used compromisedPasswordChecker method we changed the below admin password as "admin_@1122" and encrypted it
-                .password("{bcrypt}$2a$12$mNWjGgxh3ep8zOcoCsgl1.i6cC.a0MkfusKiQZoYobJvm3i16pXY6")
-                .authorities("admin")
-                .build();
-        return new InMemoryUserDetailsManager(user, admin);
+    public UserDetailsService userDetailsService(DataSource dataSource) {
+//        the below code is commenting because in section 4 we created userDetails inside the database so its not necessary to use this code anymore
+
+//        UserDetails user = User.withUsername("user")
+//                .password("{noop}user_@12345")
+//                .authorities("read")
+//                .build();
+//        UserDetails admin = User.withUsername("admin")
+////                the below password is encrypted Hash code of BCrypt and used prefix {bcrypt} its good practice to use prefix
+////                as we used compromisedPasswordChecker method we changed the below admin password as "admin_@1122" and encrypted it
+//                .password("{bcrypt}$2a$12$mNWjGgxh3ep8zOcoCsgl1.i6cC.a0MkfusKiQZoYobJvm3i16pXY6")
+//                .authorities("admin")
+//                .build();
+
+//        instead of returning below code as we're using jdbc dependency, so we can return other code as returned below the commented code
+
+//        return new InMemoryUserDetailsManager(user, admin);
+
+        return new JdbcUserDetailsManager(dataSource);
     }
 
     @Bean
